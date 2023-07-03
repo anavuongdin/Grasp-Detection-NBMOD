f06779b7f2ef6363aeb2b54f8cfea8b06401710b
anavuongdin <timeskipfor8months@gmail.com>
2023-07-03 10:08:51 +0700

    Dev

--- setup_pipe.py
+++ setup_pipe.py
@@ -9,9 +9,9 @@ import torch
 
 from fairseq import utils, tasks
 from fairseq import checkpoint_utils
-from .OFA.utils.eval_utils import eval_step
-from .OFA.tasks.mm_tasks.refcoco import RefcocoTask
-from .OFA.models.ofa import OFAModel
+from ..OFA.utils.eval_utils import eval_step
+from ..OFA.tasks.mm_tasks.refcoco import RefcocoTask
+from ..OFA.models.ofa import OFAModel
 from PIL import Image
 
 from shapely.geometry import Polygon

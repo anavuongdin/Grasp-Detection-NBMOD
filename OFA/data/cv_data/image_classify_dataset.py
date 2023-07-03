cbaadf7af6b4b6927c1106a31cfd49ca9d2eabef
anavuongdin <timeskipfor8months@gmail.com>
2023-07-03 10:27:51 +0700

    Test

--- OFA/data/cv_data/image_classify_dataset.py
+++ OFA/data/cv_data/image_classify_dataset.py
@@ -14,12 +14,12 @@ import torch
 import base64
 from torchvision import transforms
 from timm.data import create_transform
-from utils.vision_helper import RandomAugment
+from OFA.utils.vision_helper import RandomAugment
 
 from PIL import Image, ImageFile
 
-from data import data_utils
-from data.ofa_dataset import OFADataset
+from OFA.data import data_utils
+from OFA.data.ofa_dataset import OFADataset
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 ImageFile.MAX_IMAGE_PIXELS = None

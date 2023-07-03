06b142ff4f3986a175fc53e74a76c8866d48eee8
anavuongdin <timeskipfor8months@gmail.com>
2023-07-03 10:53:47 +0700

    Test

--- setup_pipe.py
+++ setup_pipe.py
@@ -6,6 +6,7 @@ import numpy
 
 from diffusers import StableDiffusionPipeline
 import torch
+from torchvision import transforms
 
 from fairseq import utils, tasks
 from fairseq import checkpoint_utils
@@ -43,3 +44,98 @@ sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
 
 predictor = SamPredictor(sam)
+
+# Setup OFA
+# Load pretrained ckpt & config
+overrides={"bpe_dir":"OFA/utils/BPE"}
+models, cfg, task = checkpoint_utils.load_model_ensemble_and_task(
+        utils.split_paths('OFA/checkpoints/refcocog.pt'),
+        arg_overrides=overrides
+    )
+
+cfg.common.seed = 7
+cfg.generation.beam = 5
+cfg.generation.min_len = 4
+cfg.generation.max_len_a = 0
+cfg.generation.max_len_b = 4
+cfg.generation.no_repeat_ngram_size = 3
+
+# Fix seed for stochastic decoding
+if cfg.common.seed is not None and not cfg.generation.no_seed_provided:
+    np.random.seed(cfg.common.seed)
+    utils.set_torch_seed(cfg.common.seed)
+
+# Move models to GPU
+for model in models:
+    model.eval()
+    if use_fp16:
+        model.half()
+    if use_cuda and not cfg.distributed_training.pipeline_model_parallel:
+        model.cuda()
+    model.prepare_for_inference_(cfg)
+
+# Initialize generator
+generator = task.build_generator(models, cfg.generation)
+
+# Image transform
+mean = [0.5, 0.5, 0.5]
+std = [0.5, 0.5, 0.5]
+
+patch_resize_transform = transforms.Compose([
+    lambda image: image.convert("RGB"),
+    transforms.Resize((cfg.task.patch_image_size, cfg.task.patch_image_size), interpolation=Image.BICUBIC),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std),
+])
+
+# Text preprocess
+bos_item = torch.LongTensor([task.src_dict.bos()])
+eos_item = torch.LongTensor([task.src_dict.eos()])
+pad_idx = task.src_dict.pad()
+def encode_text(text, length=None, append_bos=False, append_eos=False):
+    s = task.tgt_dict.encode_line(
+        line=task.bpe.encode(text.lower()),
+        add_if_not_exist=False,
+        append_eos=False
+    ).long()
+    if length is not None:
+        s = s[:length]
+    if append_bos:
+        s = torch.cat([bos_item, s])
+    if append_eos:
+        s = torch.cat([s, eos_item])
+    return s
+
+# Construct input for refcoco task
+patch_image_size = cfg.task.patch_image_size
+def construct_sample(image: Image, text: str):
+    w, h = image.size
+    w_resize_ratio = torch.tensor(patch_image_size / w).unsqueeze(0)
+    h_resize_ratio = torch.tensor(patch_image_size / h).unsqueeze(0)
+    patch_image = patch_resize_transform(image).unsqueeze(0)
+    patch_mask = torch.tensor([True])
+    src_text = encode_text(' which region does the text " {} " describe?'.format(text), append_bos=True, append_eos=True).unsqueeze(0)
+    src_length = torch.LongTensor([s.ne(pad_idx).long().sum() for s in src_text])
+    sample = {
+        "id":np.array(['42']),
+        "net_input": {
+            "src_tokens": src_text,
+            "src_lengths": src_length,
+            "patch_images": patch_image,
+            "patch_masks": patch_mask,
+        },
+        "w_resize_ratios": w_resize_ratio,
+        "h_resize_ratios": h_resize_ratio,
+        "region_coords": torch.randn(1, 4)
+    }
+    return sample
+
+# Function to turn FP32 to FP16
+def apply_half(t):
+    if t.dtype is torch.float32:
+        return t.to(dtype=torch.half)
+    return t
+
+width = 416
+height = 416
+dim = (width, height)

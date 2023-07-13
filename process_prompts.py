dbe357d6b60dccd5e5521ecafeb85048d1338f51
anavuongdin <timeskipfor8months@gmail.com>
2023-07-13 08:54:01 +0700

    Change file name

--- process_prompts.py
+++ process_prompts.py
@@ -2,7 +2,7 @@ import os
 import pickle
 
 def read_prompts(file_name):
-    with open('file_name', 'rb') as f:
+    with open(file_name, 'rb') as f:
         x = pickle.load(f)
         x = x.strip('\n')
 
@@ -12,4 +12,3 @@ def read_prompts(file_name):
         queries = queries.split(' ')
         queries = list(map(lambda x: x.strip(','), queries))
         yield prompt, queries
-

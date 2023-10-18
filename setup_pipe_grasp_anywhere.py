95c2d8a8411225fa59a32154a867e5790f99bc4b
An Vuong <andvg3@gmail.com>
2023-10-18 15:09:52 +0700

    Add device

--- setup_pipe_grasp_anywhere.py
+++ setup_pipe_grasp_anywhere.py
@@ -52,7 +52,7 @@ from segment_anything import sam_model_registry, SamPredictor
 # sam_checkpoint = "sam_vit_h_4b8939.pth"
 # model_type = "vit_h"
 
-# device = "cuda"
+device = "cuda"
 
 # sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 # sam.to(device=device)
